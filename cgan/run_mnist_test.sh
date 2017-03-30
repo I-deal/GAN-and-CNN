2c9a76d72f3856d6f0335db19d420804d2a6f14c
Ubuntu <ubuntu@ip-172-31-43-103.us-west-2.compute.internal>
2017-03-30 06:45:24 +0000

    test4

---
+++ cgan/run_mnist_test.sh
@@ -0,0 +1,1 @@
+python3 main.py --dataset mnist --batch_size=169 --input_height=28 --output_height=28 --c_dim=1
---
+++ cgan/run_mnist_train.sh
@@ -0,0 +1,1 @@
+python3 main.py --dataset mnist --epoch=10 --batch_size=169 --input_height=28 --output_height=28 --c_dim=1 --is_train
